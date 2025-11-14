4714e29941f546b850d560bfc95c5c7c598e60f3
Builtbyjb <awotideajibola@gmail.com>
2025-11-14 12:45:54 -0500

    convert add digits into strings

--- calasm.s
+++ calasm.s
@@ -1,38 +1,58 @@
-
 .global _main
 .align 2
 .text
 
 _main:
-    mov W0, #5
-    add W0, W0, #4
+    mov     w0, #12
+    add     w0, w0, #7
+    bl      _int_to_string
 
-
-    // Convert all the values in the W0 register to strings 
-    add W0, W0, #'0'
-
-    strb W0, [sp, #-1]!
-
-
-    // Print out a value stored on the stack 
-    mov X0, #1
-    mov X1, sp
-    mov X2, #1
-    mov X16, #4
-    svc #0x80
+_print_value:
+    mov     x0, #1
+    mov     x2, x19
+    mov     x16, #4
+    svc     #0x0
 
 _print_newline:
-    mov X0, #1
-    adr X1, newline
-    mov X2, #1
-    mov X16, #4
-    svc #0x80
+    mov     x0, #1
+    adr     x1, newline
+    mov     x2, #1
+    mov     x16, #4
+    svc     #0x0
 
 _exit:
-    mov X0, #0
-    mov X16, #1
-    svc #0x80
+    mov     x0, #0
+    mov     x16, #1
+    svc     #0x0
 
- newline: 
-    .ascii "\n"
+ _int_to_string:
+    mov     x19, #0
+    mov     x2, #10
+    sub     sp, sp, #64
+
+ _convert_loop:
+    udiv    w4, w0, w2
+    msub    w5, w4, w2, w0
+    add     w5, w5, #'0'
+    strb    w5, [sp, x19]
+    add     x19, x19, #1
+    mov     w0, w4
+    cbnz    w0, _convert_loop
+
+    mov     x4, #0
+    sub     x18, x19, #1
+
+ _copy:
+    ldrb    w5, [sp, x18]
+    strb    w5, [x1, x4]
+    add     x4, x4, #1
+    sub     x18, x18, #1
+    cmp     x4, x19
+    b.lt    _copy
+
+    add     sp, sp, #64
+    ret
+
+ newline:
+    .ascii  "\n"
 
